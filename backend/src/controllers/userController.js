e3cb6bab674ead53661c0b501b06a2104401a3d9
Deivid Moura <dejesusdemouradeivid@gmail.com>
2026-02-07 01:27:27 -0300

    feat: senha agora é criptografada
    API básica de CRUD para API com proteção mínima de segurança

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,6 @@
 import { prisma } from "../config/prisma.js";
+import bcrypt from "bcryptjs";
+
 
 export const registerUser = async (req, res) => {
   try {
@@ -12,15 +14,22 @@ export const registerUser = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await prisma.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
       },
     });
 
-    return res.status(201).json(user);
+    return res.status(201).json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: user.createdAt,
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
