c7f6f2788d52659d5d980769259f0d767fe173f5
Deivid Moura <dejesusdemouradeivid@gmail.com>
2026-02-07 01:48:13 -0300

    feat: lOGIN com verificação de senha
    Backend capaz de Comparar senha digitada com hash, Gerar JWT, Retornar token

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,7 @@
 import { prisma } from "../config/prisma.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 
 
 export const registerUser = async (req, res) => {
@@ -29,6 +31,44 @@ export const registerUser = async (req, res) => {
       name: user.name,
       email: user.email,
       createdAt: user.createdAt,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    return res.status(200).json({
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
     });
   } catch (error) {
     console.error(error);
